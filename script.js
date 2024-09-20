c341a1e4fc96ffaf91b61cd2d7b3684382e750be
Clayton Sweeten <cjsweeten101@gmail.com>
2024-09-20 12:54:17 -0600

    Change grid size on button click

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 const container = document.querySelector("#container");
-let row_size = 16;
-let grid_size = row_size * row_size;
+let current_row_size = 16;
+
+const onGridChangeClick = function (e) {
+    let new_row_size;
+    new_row_size = prompt("Enter a new size", "16");
+
+    while (new_row_size > 100) {
+        new_row_size = prompt("Sorry value too big, please enter another", new_row_size);
+    }
+    //first delete current grid
+    deleteGrid();
+    
+    createGrid(new_row_size);
+}
 
 const onDivMouseEnter = function (e) {
     e.target.style.backgroundColor = "rgb(62, 70, 78)";
 }
 
-let current_row = document.createElement("div");
-container.appendChild(current_row);
-for ( i = 1; i <= grid_size; i++ ) {
-    new_div = document.createElement("div");
-    current_row.appendChild(new_div);
-    new_div.addEventListener("mouseenter", onDivMouseEnter);
-    if ( i % row_size === 0 && i != grid_size ) {
-        current_row = document.createElement("div");
-        container.appendChild(current_row);
+const createGrid = function (row_size) {
+    let total_div_count = row_size* row_size;
+    let current_row = document.createElement("div");
+    container.appendChild(current_row);
+
+    for ( i = 1; i <= total_div_count; i++ ) {
+        new_div = document.createElement("div");
+        current_row.appendChild(new_div);
+        new_div.addEventListener("mouseenter", onDivMouseEnter);
+
+        if ( i % row_size === 0 && i != total_div_count ) {
+            current_row = document.createElement("div");
+            container.appendChild(current_row);
+        }
     }
 }
+
+const deleteGrid = function () {
+    rows = container.querySelectorAll("div");
+    console.log(rows)
+
+    for ( i = 0; i < rows.length - 1; i++ ) {
+        rows[i].remove();
+    }
+}
+
+createGrid(current_row_size);
