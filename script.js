29204949854ff49b6809fb850499821dfe76542a
Clayton Sweeten <cjsweeten101@gmail.com>
2024-09-20 11:50:22 -0600

    fix bug and change grid size

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const container = document.querySelector("#container");
-let grid_size = 16;
-let row_size = 4;
+let row_size = 16;
+let grid_size = row_size * row_size;
 
 let current_row = document.createElement("div");
 container.appendChild(current_row);
 for ( i = 1; i <= grid_size; i++ ) {
     new_div = document.createElement("div");
     current_row.appendChild(new_div);
-    if ( i % row_size === 0 && i != 0 ) {
+    if ( i % row_size === 0 && i != grid_size ) {
         current_row = document.createElement("div");
         container.appendChild(current_row);
     }
